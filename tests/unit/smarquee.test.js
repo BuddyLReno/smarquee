38adfd3de265ca357ae0c3540e0458bd181a8d39
Buddy Reno <buddy.reno@daveramsey.com>
2019-03-19 21:04:31 -0500

    Setup jest

---
+++ tests/unit/smarquee.test.js
@@ -0,0 +1,23 @@
+import Smarquee from '../../src/smarquee';
+
+test('element with id smarquee is selected in constructor', () => {
+  document.body.innerHTML = `
+  <div id="smarquee">Some title</div>
+  `;
+
+  const smarquee = new Smarquee();
+  expect(smarquee.marqueeContainer.id).toBe('smarquee');
+});
+
+test('preselected element is set to marqueeContainer on init', () => {
+  document.body.innerHTML = `
+  <div id="smarquee2">a test title</div>
+  `;
+
+  const smarquee = new Smarquee({
+    element: document.querySelector('#smarquee2')
+  });
+  console.log(smarquee.marqueeContainer.id);
+  expect(smarquee.marqueeContainer.id).toBe('smarquee2');
+  expect(smarquee.marqueeContainer.innerHTML).toBe('a test title');
+});
