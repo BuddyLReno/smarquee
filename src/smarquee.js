671ce5cf1dfc6bebd07b7e2b37eb310e42a73f7f
Buddy Reno <buddy.reno@daveramsey.com>
2019-03-20 07:37:56 -0500

    Add duplicated element. Add calculation for animation

--- src/smarquee.js
+++ src/smarquee.js
@@ -5,6 +5,12 @@ export default class Smarquee {
   constructor(options = {}) {
     this.settings = Object.assign({}, defaults, options);
 
+    this.animationCalulations = {
+      distance: 0,
+      animatedDistance: 0,
+      time: 0
+    }
+
     if (this.settings.element) {
       this.marqueeContainer = this.settings.element;
     } else {
@@ -12,7 +18,38 @@ export default class Smarquee {
     }
   }
 
-  init() {
-
+  get needsMarquee() {
+    return this.marqueeContainer.scrollWidth > this.marqueeContainer.clientWidth;
   }
+
+  init() {
+    if (this.needsMarquee) {
+      this.createScrollTitle();
+      this.setAnimationProperties();
+    }
+  }
+
+  createScrollTitle() {
+    const titleContent = this.marqueeContainer.innerHTML;
+    const scrollTitle = `<span data-marquee-scroll-wrapper>${titleContent}<span data-smarquee-scroll-title>${titleContent}</span></span>`;
+
+    this.marqueeContainer.innerHTML = scrollTitle;
+    this.scrollWrapper = this.marqueeContainer.querySelector('[data-marquee-scroll-wrapper]');
+  }
+
+  setAnimationProperties() {
+    const distance = this.marqueeContainer.scrollWidth;
+    const finalDistance = distance / 2 + 24;
+    const time = distance / this.settings.velocity;
+
+    this.marqueeContainer.style.setProperty('--time', `${time}s`);
+    this.marqueeContainer.style.setProperty('--distance', `-${finalDistance}px`);
+
+    this.animationCalulations.distance = distance;
+    this.animationCalulations.animatedDistance = finalDistance;
+    this.animationCalulations.time = time;
+
+    this.scrollWrapper.classList.add('animate');
+  }
+
 }
--- tests/unit/smarquee.test.js
+++ tests/unit/smarquee.test.js
@@ -5,8 +5,8 @@ test('element with id smarquee is selected in constructor', () => {
   <div id="smarquee">Some title</div>
   `;
 
-  const smarquee = new Smarquee();
-  expect(smarquee.marqueeContainer.id).toBe('smarquee');
+  const subject = new Smarquee();
+  expect(subject.marqueeContainer.id).toBe('smarquee');
 });
 
 test('preselected element is set to marqueeContainer on init', () => {
@@ -14,10 +14,54 @@ test('preselected element is set to marqueeContainer on init', () => {
   <div id="smarquee2">a test title</div>
   `;
 
-  const smarquee = new Smarquee({
+  const subject = new Smarquee({
     element: document.querySelector('#smarquee2')
   });
 
-  expect(smarquee.marqueeContainer.id).toBe('smarquee2');
-  expect(smarquee.marqueeContainer.innerHTML).toBe('a test title');
+  expect(subject.marqueeContainer.id).toBe('smarquee2');
+  expect(subject.marqueeContainer.innerHTML).toBe('a test title');
+});
+
+test('needsMarquee returns true when when scrollWidth is larger than displayed width', () => {
+  document.body.innerHTML = `
+  <h1 id="smarquee">This is a long test title that needs a marquee</h1>
+  `;
+
+  let subject = new Smarquee();
+  jest.spyOn(subject.marqueeContainer, 'scrollWidth', 'get').mockReturnValue(100);
+  jest.spyOn(subject.marqueeContainer, 'clientWidth', 'get').mockReturnValue(10);
+  expect(subject.needsMarquee).toBe(true);
+});
+
+test('needsMarquee returns false when when scrollWidth is equal or less than displayed width', () => {
+  document.body.innerHTML = `
+  <h1 id="smarquee">This is a long test title that needs a marquee</h1>
+  `;
+
+  let subject = new Smarquee();
+  jest.spyOn(subject.marqueeContainer, 'scrollWidth', 'get').mockReturnValue(10);
+  jest.spyOn(subject.marqueeContainer, 'clientWidth', 'get').mockReturnValue(10);
+  expect(subject.needsMarquee).toBe(false);
+});
+
+test('createScrollingContent adds duplicated content for scrolling', () => {
+  document.body.innerHTML = `
+  <h1 id="smarquee">This is a test title.</h1>
+  `;
+
+  let subject = new Smarquee();
+  subject.createScrollTitle();
+  expect(subject.marqueeContainer.innerHTML).toBe('This is a test title. <span data-smarquee-scroll-title="">This is a test title.</span>');
+});
+
+test('init adds scrolling content if needsMarquee is true', () => {
+  document.body.innerHTML = `
+  <h1 id="smarquee">This is a long test title that needs a marquee</h1>
+  `;
+
+  let subject = new Smarquee();
+  jest.spyOn(subject.marqueeContainer, 'scrollWidth', 'get').mockReturnValue(100);
+  jest.spyOn(subject.marqueeContainer, 'clientWidth', 'get').mockReturnValue(10);
+  subject.init();
+  expect(subject.marqueeContainer.innerHTML).toBe('This is a long test title that needs a marquee <span data-smarquee-scroll-title="">This is a long test title that needs a marquee</span>');
 });
